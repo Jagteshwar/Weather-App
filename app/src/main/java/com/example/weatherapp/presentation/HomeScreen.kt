abd32abf956395bbd923bc6d4bb8f458fbb115fd
Jagteshwar <jagteshwars3@gmail.com>
2025-04-08 15:45:30 -0400

    refactoring

--- app/src/main/java/com/example/weatherapp/presentation/HomeScreen.kt
+++ app/src/main/java/com/example/weatherapp/presentation/HomeScreen.kt
@@ -5,12 +5,8 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.paint
 import androidx.compose.ui.graphics.Brush
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
-import com.example.weatherapp.R
 import com.example.weatherapp.common.Constants.mainBackground
 import com.example.weatherapp.ui.theme.WeatherAppTheme
 
