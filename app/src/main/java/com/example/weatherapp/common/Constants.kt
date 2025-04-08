6fbf67bd74421410aa8ca65409346b2f35aab976
Jagteshwar <jagteshwars3@gmail.com>
2025-04-08 15:44:05 -0400

    background_setup

---
+++ app/src/main/java/com/example/weatherapp/common/Constants.kt
@@ -0,0 +1,11 @@
+package com.example.weatherapp.common
+
+import androidx.compose.ui.graphics.Color
+
+object Constants {
+
+     val mainBackground = listOf(
+        Color(0xFF669900),
+        Color.White,
+    )
+}
---
+++ app/src/main/java/com/example/weatherapp/presentation/HomeScreen.kt
@@ -0,0 +1,37 @@
+package com.example.weatherapp.presentation
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.paint
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.weatherapp.R
+import com.example.weatherapp.common.Constants.mainBackground
+import com.example.weatherapp.ui.theme.WeatherAppTheme
+
+@Composable
+fun HomeScreen() {
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(
+                brush = Brush.verticalGradient(
+                    colors = mainBackground
+                    )
+                )
+            )
+
+}
+
+@Preview
+@Composable
+fun PreviewMainScreen(modifier: Modifier = Modifier) {
+    WeatherAppTheme {
+        HomeScreen()
+    }
+}
