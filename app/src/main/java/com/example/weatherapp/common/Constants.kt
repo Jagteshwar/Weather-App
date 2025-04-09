d70625bb8e28c8fdb5bb667cb0ae7339d725fe53
Jagteshwar <jagteshwars3@gmail.com>
2025-04-09 19:47:25 -0400

    dto_setup

--- app/src/main/java/com/example/weatherapp/common/Constants.kt
+++ app/src/main/java/com/example/weatherapp/common/Constants.kt
@@ -4,6 +4,9 @@ import androidx.compose.ui.graphics.Color
 
 object Constants {
 
+    const val BASE_URL = "http://api.weatherapi.com/"
+    const val API_KEY = "94075b04f8124df4aa8231422250904"
+
      val mainBackground = listOf(
         Color(0xFF669900),
         Color.White,
---
+++ app/src/main/java/com/example/weatherapp/data/remote/WeatherApi.kt
@@ -0,0 +1,15 @@
+package com.example.weatherapp.data.remote
+
+import com.example.weatherapp.data.remote.dto.TemperatureDto
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface WeatherApi {
+
+    @GET("v1/current.json")
+    suspend fun getCurrentWeather(
+        @Query("key") apiKey: String,
+        @Query("q") location: String,
+        @Query("aqi") aqi: String
+    ): TemperatureDto
+}
